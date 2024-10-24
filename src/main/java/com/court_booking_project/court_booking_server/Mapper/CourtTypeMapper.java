c118ba91c6901fda9b203f87a02cccdf7b7bb6bb
vihao <tranvihao40@gmail.com>
2024-10-24 22:12:19 +0700

    feat(mapper, controller): add court type

---
+++ src/main/java/com/court_booking_project/court_booking_server/Mapper/CourtTypeMapper.java
@@ -0,0 +1,18 @@
+package com.court_booking_project.court_booking_server.Mapper;
+
+import com.court_booking_project.court_booking_server.dto.Request.court_type.CreateCourtTypeRequest;
+import com.court_booking_project.court_booking_server.dto.Request.court_type.UpdateCourtTypeRequest;
+import com.court_booking_project.court_booking_server.dto.Response.CourtTypeResponse;
+import com.court_booking_project.court_booking_server.entity.CourtType;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface CourtTypeMapper {
+
+    CourtType convertCreateDTOtoEntity(CreateCourtTypeRequest request);
+
+    void convertUpdateDTOtoEntity(@MappingTarget CourtType courtType , UpdateCourtTypeRequest request);
+
+    CourtTypeResponse convertEntityToDTO(CourtType entity);
+}
