add34e1bc95944371afe93d4e102d620e7d10650
vihao <tranvihao40@gmail.com>
2024-10-24 23:06:59 +0700

    feat(dto): add annotation notnull , notempty to variables

--- src/main/java/com/court_booking_project/court_booking_server/dto/Request/zalopay/ZaloPayRequestCreatePaymentDTO.java
+++ src/main/java/com/court_booking_project/court_booking_server/dto/Request/zalopay/ZaloPayRequestCreatePaymentDTO.java
@@ -1,5 +1,8 @@
 package com.court_booking_project.court_booking_server.dto.Request.zalopay;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,11 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class ZaloPayRequestCreatePaymentDTO {
+
+    @NotNull(message = "totalPrice cannot be null")
+    @Positive // totalPrice must be greater than 0
     Integer totalPrice;
+
+    @NotEmpty(message = "userName cannot be an empty string")
     String userName;
 }
