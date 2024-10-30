588b3c4edbb88c61225194f1f07ca3ed226cb5b7
vihao <tranvihao40@gmail.com>
2024-10-30 22:50:48 +0700

    docs: add title, version, description for swagger api

--- src/main/java/com/court_booking_project/court_booking_server/config/SwaggerConfig.java
+++ src/main/java/com/court_booking_project/court_booking_server/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.court_booking_project.court_booking_server.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,10 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Court Booking System")
+                        .version("1.0")
+                        .description("API documentation for the Court Booking System"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer_Token", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
