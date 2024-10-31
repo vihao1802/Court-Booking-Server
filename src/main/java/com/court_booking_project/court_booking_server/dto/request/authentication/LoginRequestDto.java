63bc17b14e297c7e5463dabba687fe83a55e1ecf
hnoga-n <hoanghuynguyen711@gmail.com>
2024-10-31 09:26:44 +0700

    chore

--- src/main/java/com/court_booking_project/court_booking_server/dto/request/authentication/LoginRequestDto.java
+++ src/main/java/com/court_booking_project/court_booking_server/dto/request/authentication/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.court_booking_project.court_booking_server.dto.request.authentication;
 
+import com.court_booking_project.court_booking_server.custom_annotation.PasswordConstraint;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,10 +11,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginRequestDto {
-
     @Schema(description = "Email", example = "admin@admin.com")
     String email;
-
 
     @Schema(description = "Password", example = "admin")
     String password;
