c90f9d627ffdc0cdda3b54e2827ac391342a43fc
Huchuynh <tuanhaomach@gmail.com>
2024-11-12 07:13:26 +0700

    feature: upgrade momo and config

--- src/main/java/com/court_booking_project/court_booking_server/CourtBookingServerApplication.java
+++ src/main/java/com/court_booking_project/court_booking_server/CourtBookingServerApplication.java
@@ -2,18 +2,20 @@ package com.court_booking_project.court_booking_server;
 
 import com.court_booking_project.court_booking_server.config.ZaloPayConfig;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ZaloPayConfig.class)
+@Slf4j
 public class CourtBookingServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourtBookingServerApplication.class, args);
 	}
 	@PostConstruct
 	public void printSwaggerUrl() {
-		System.out.println("Swagger UI available at: http://localhost:8080/swagger-ui/index.html");
+		log.info("Swagger UI available at: http://localhost:8080/swagger-ui/index.html");
 	}
 }
