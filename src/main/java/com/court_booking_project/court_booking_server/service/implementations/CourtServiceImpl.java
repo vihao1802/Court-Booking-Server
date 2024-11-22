71b35e97368b28948cab65db4ce2256d061a6e21
vihao <tranvihao40@gmail.com>
2024-11-22 15:21:50 +0700

    refactor(service): add select date time booking feature

--- src/main/java/com/court_booking_project/court_booking_server/service/implementations/CourtServiceImpl.java
+++ src/main/java/com/court_booking_project/court_booking_server/service/implementations/CourtServiceImpl.java
@@ -1,16 +1,19 @@
 package com.court_booking_project.court_booking_server.service.implementations;
 
+import com.court_booking_project.court_booking_server.constant.ReservationState;
 import com.court_booking_project.court_booking_server.dto.request.court.CreateCourtRequest;
 import com.court_booking_project.court_booking_server.dto.request.court.UpdateCourtRequest;
 import com.court_booking_project.court_booking_server.dto.response.court.CourtResponse;
 import com.court_booking_project.court_booking_server.entity.Court;
 import com.court_booking_project.court_booking_server.entity.CourtImage;
+import com.court_booking_project.court_booking_server.entity.Reservation;
 import com.court_booking_project.court_booking_server.exception.AppException;
 import com.court_booking_project.court_booking_server.exception.ErrorCode;
 import com.court_booking_project.court_booking_server.mapper.CourtMapper;
 import com.court_booking_project.court_booking_server.repository.ICourtImageRepository;
 import com.court_booking_project.court_booking_server.repository.ICourtRepository;
 import com.court_booking_project.court_booking_server.repository.ICourtTypeRepository;
+import com.court_booking_project.court_booking_server.repository.IReservationRepository;
 import com.court_booking_project.court_booking_server.service.interfaces.ICourtService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +24,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +36,7 @@ public class CourtServiceImpl implements ICourtService {
     ICourtRepository courtRepository;
     ICourtTypeRepository courtTypeRepository;
     ICourtImageRepository courtImageRepository;
+    IReservationRepository reservationRepository;
     CourtMapper courtMapper;
 
     @Override
@@ -80,6 +85,75 @@ public class CourtServiceImpl implements ICourtService {
         courtMapper.convertUpdateDTOtoEntity(court, request);
         courtRepository.save(court);
         return courtMapper.convertEntityToDTO(court);
+    }
+
+    @Override
+    public List<String> getAvailableDate(String courtId) {
+        List<String> availableDays = new ArrayList<>();
+        
+        LocalDate today = LocalDate.now();
+        int lastDayOfRange = 3; // range day is 3 from now
+
+        for (int i = 0; i <= lastDayOfRange; i++) {
+            LocalDate date = today.plusDays(i);
+            if(checkAvailableDate(date,courtId)) {
+                availableDays.add(date.toString());
+            }
+        }
+        
+        return availableDays;
+    }
+
+    private List<String> calculateAvailableHours(LocalDate date, String courtId) {
+        List<String> availableHours = new ArrayList<>();
+        for (int i = 8; i <= 24; i++) {
+            availableHours.add(String.valueOf(i));
+        }
+
+        List<ReservationState> states = Arrays.asList(ReservationState.PENDING, ReservationState.SUCCESS);
+        List<Reservation> reservations = reservationRepository.findByReservationDateAndCourtIdAndReservationStates(date, courtId, states);
+
+        for (Reservation res : reservations) {
+            int checkIn = Integer.parseInt(res.getCheckInTime());
+            int checkOut = Integer.parseInt(res.getCheckOutTime());
+
+            for (int i = checkIn; i < checkOut; i++) {
+                availableHours.remove(String.valueOf(i));
+            }
+        }
+
+        return availableHours;
+    }
+
+    public boolean checkAvailableDate(LocalDate date, String courtId) {
+        List<String> availableHours = calculateAvailableHours(date, courtId);
+        return !availableHours.isEmpty();
+    }
+
+    public List<String> getAvailableHours(String courtId,String date) {
+        LocalDate parsedDate = LocalDate.parse(date);
+        return calculateAvailableHours(parsedDate, courtId);
+    }
+
+    public List<String> getUnavailableHours(String courtId, String date) {
+        LocalDate parsedDate = LocalDate.parse(date);
+
+        List<ReservationState> states = Arrays.asList(ReservationState.PENDING, ReservationState.SUCCESS);
+        List<Reservation> reservations = reservationRepository.findByReservationDateAndCourtIdAndReservationStates(parsedDate, courtId, states);
+
+        Set<String> notAvailableHoursSet = new HashSet<>();
+
+        for (Reservation res : reservations) {
+            int checkIn = Integer.parseInt(res.getCheckInTime());
+            int checkOut = Integer.parseInt(res.getCheckOutTime());
+
+            for (int i = checkIn; i <= checkOut; i++) {
+                notAvailableHoursSet.add(String.valueOf(i));
+            }
+        }
+
+        return new ArrayList<>(notAvailableHoursSet);
+
     }
 
 
--- src/main/java/com/court_booking_project/court_booking_server/service/interfaces/ICourtService.java
+++ src/main/java/com/court_booking_project/court_booking_server/service/interfaces/ICourtService.java
@@ -3,10 +3,10 @@ package com.court_booking_project.court_booking_server.service.interfaces;
 import com.court_booking_project.court_booking_server.dto.request.court.CreateCourtRequest;
 import com.court_booking_project.court_booking_server.dto.request.court.UpdateCourtRequest;
 import com.court_booking_project.court_booking_server.dto.response.court.CourtResponse;
-import com.court_booking_project.court_booking_server.entity.Court;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface ICourtService {
@@ -15,4 +15,5 @@ public interface ICourtService {
     CourtResponse get(String id);
     CourtResponse add(CreateCourtRequest request);
     CourtResponse update(String id, UpdateCourtRequest request);
+    List<String> getAvailableDate(String id);
 }
